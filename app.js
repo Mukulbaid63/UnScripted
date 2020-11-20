5e7752a5a3faca997b96b73b2e1ed6d0bd8a0613
Mukulbaid63 <mukulbaid63@gmail.com>
2020-11-21 03:32:55 +0545

    sab kuch almost update krdia

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ $(window).scroll(function() {
       $('nav').removeClass('transparent');
     }
   });
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+  } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+   } 
+  });
+}
+
+new TypeIt("#typeit", {
+  strings: "Revolutionizing Open Source.",
+  speed: 100,
+  waitUntilVisible: true
+}).go();
